d718b861f5e861b5ccc9d026e08bde1e1c27cd46
IYakRiyadh <Riyadhruzaini2507@gmail.com>
2023-08-18 12:25:26 +0700

    Go Update

--- lib/pages/form/login.dart
+++ lib/pages/form/login.dart
@@ -17,12 +17,32 @@ class _LoginFormState extends State<LoginForm> {
             child: Column(
               children: [
                 MaterialButton(
+                  minWidth: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  color: const Color(0xFF0C8913),
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: const BorderSide(
+                        width: 2.50,
+                        color: Color(0xFF0C8913),
+                      )),
+                  child: const Text('Masuk sebagai Customer'),
                   onPressed: () {},
-                  child: Text('Masuk sebagai Customer'),
                 ),
                 MaterialButton(
+                  minWidth: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.transparent,
+                  textColor: const Color(0xFF0C8913),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: const BorderSide(
+                        width: 2.50,
+                        color: Color(0xFF0C8913),
+                      )),
+                  child: const Text('Masuk sebagai Driver'),
                   onPressed: () {},
-                  child: Text('Masuk sebagai Driver'),
                 ),
               ],
             ),
