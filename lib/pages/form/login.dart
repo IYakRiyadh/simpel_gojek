4cc02188372fa0582132b8de832edcfe45064b9b
IYakRiyadh <Riyadhruzaini2507@gmail.com>
2023-08-23 08:23:13 +0700

    Dial Code

--- lib/pages/form/login.dart
+++ lib/pages/form/login.dart
@@ -22,87 +22,89 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Align(
-              //   alignment: Alignment.centerLeft,
-              //   child: Padding(
-              //     padding: const EdgeInsets.all(20.0),
-              //     child: Flex(
-              //       direction: Axis.vertical,
-              //       children: [
-              //         Container(
-              //           width: 80.0,
-              //           height: 80.0,
-              //           decoration: BoxDecoration(
-              //             borderRadius: BorderRadius.circular(15.0),
-              //             color: Colors.lightBlue,
-              //           ),
-              //         ),
-              //         Container(child: Text('data \nsaya'))
-              //       ],
-              //     ),
-              //   ),
-              // ),
-              SizedBox(
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    prefixStyle: TextStyle(color: Colors.black),
-                    prefix: Container(
-                      width: 80,
-                      padding: EdgeInsets.only(right: 10),
-                      margin: EdgeInsets.only(right: 10),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          right: BorderSide(
-                            width: 2.0,
-                            color: Color(0xFF0C8913),
-                          ),
-                        ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        padding: EdgeInsets.all(20.0),
+        child: Flex(
+          direction: Axis.vertical,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Align(
+            //   alignment: Alignment.centerLeft,
+            //   child: Padding(
+            //     padding: const EdgeInsets.all(20.0),
+            //     child: Flex(
+            //       direction: Axis.vertical,
+            //       children: [
+            //         Container(
+            //           width: 80.0,
+            //           height: 80.0,
+            //           decoration: BoxDecoration(
+            //             borderRadius: BorderRadius.circular(15.0),
+            //             color: Colors.lightBlue,
+            //           ),
+            //         ),
+            //         Container(child: Text('data \nsaya'))
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            SizedBox(
+              height: 36,
+              child: Row(
+                // direction: Axis.horizontal,
+                children: [
+                  Text('data'),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 80.0,
+                  child: DropdownButtonFormField(
+                    items: data.map((e) {
+                      return DropdownMenuItem(
+                        child: Text.rich(e['dial_code']),
+                        value: e,
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {});
+                      valData = value;
+                    },
+                    value: valData,
+                  ),
+                ),
+                const SizedBox(width: 10.0),
+                Expanded(
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: 'Ketik Nomer',
+                      // contentPadding: const EdgeInsets.only(
+                      //   left: 15.0,
+                      // ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 3.5, color: Color(0xFF0C8913)),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      child: DropdownButtonFormField(
-                        elevation: 0,
-                        isExpanded: true,
-                        isDense: false,
-                        items: data.map((e) {
-                          return DropdownMenuItem(
-                              value: e,
-                              child: Text(
-                                e['dial_code'],
-                              ));
-                        }).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            valData = value;
-                          });
-                        },
-                        value: valData,
+                      border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2.6, color: Colors.black87),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                    ),
-                    hintText: 'Ketik Nomer',
-                    contentPadding: const EdgeInsets.only(
-                      left: 15.0,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 3.5, color: Color(0xFF0C8913)),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 2.0, color: Colors.black87),
-                      borderRadius: BorderRadius.circular(10.0),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
